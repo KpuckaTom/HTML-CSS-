8d4be216c6e0a42eb7f59a963ecb784b69df3088
KpuckaTom <92994580+KpuckaTom@users.noreply.github.com>
2022-05-11 09:33:15 +0300

    Add files via upload

---
+++ ЛР№11/Keks_calcul.js
@@ -0,0 +1,8 @@
+var timeInMinutes;
+timeInMinutes = prompt(timeInMinutes, []);
+var jumps;
+jumps = (timeInMinutes / 10) * 3;
+
+var message;
+message = 'За ' + timeInMinutes + ' минут в соцсетях полагается ' + jumps + ' прыжков.';
+console.log(message);
